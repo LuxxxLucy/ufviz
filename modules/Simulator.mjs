8abc689e127507820dcf02f119ef07702d3b861c
LU Jialin <luxxxlucy@gmail.com>
2023-11-08 23:52:24 +0800

    prune code

--- modules/Simulator.mjs
+++ modules/Simulator.mjs
@@ -1,24 +1,13 @@
-import { Rulial } from "./Rulial.mjs";
-import { Rewriter } from "./Rewriter.mjs";
 import { Graph3D } from "./graph3d/Graph3D.mjs";
-import { HDC } from "./HDC.mjs";
-import { SpriteText } from "./SpriteText.mjs";
+import { SpriteText } from "./text/SpriteText.mjs";
 
 /**
-* @class User interface for rewriter
-* @author Mika Suominen
-* @author Tuomas Sorakivi
+* @class User interface for Simulator
+*
+* Note: skeleton originally adopted from Mika Suominen Tuomas Sorakivi who implemented this for Hypergraph Rewrite
+* https://github.com/met4citizen/Hypergraph)
 */
-class Simulator extends Rewriter {
-
-	/**
-	* Reference frame
-	* @typedef {Object} RefFrame
-	* @property {number} view Viewmode, 1 = space (default), 2 = time
-	* @property {boolean} leaves If true, show only leaves of the multiway system
-	* @property {number} branches Field for branches to show, 0 = all
-	*/
-
+class Simulator {
 	/**
 	* Creates an instance of Simulator.
 	* @param {Object} element DOM element of the canvas
@@ -26,8 +15,10 @@ class Simulator extends Rewriter {
 	* @constructor
 	*/
 	constructor(canvas,status) {
-		super();
-    this.G = new Graph3D(canvas);
+        this.EV = []; // graphs
+		this.M = []; // LHS hits as maps
+		this.reset();
+        this.G = new Graph3D(canvas);
 		this.dom = status; // DOM element for status
 
 		this.maxtokenid = -1; // Max token id shown in phase view
@@ -79,6 +70,165 @@ class Simulator extends Rewriter {
 	}
 
 	/**
+	* Reset instance.
+	* @param {Options} [opt=null]
+	*/
+	reset( opt = null ) {
+		opt = opt || {};
+
+		this.step = -1;
+		this.M.length = 0;
+		this.eventcnt = 0;
+		this.opt = {
+			evolution: 1,
+			interactions: 5,
+			timeslot: 250,
+			maxsteps: Infinity,
+			maxevents: Infinity,
+			maxtokens: Infinity,
+			noduplicates: false,
+			pathcnts: true,
+			bcoordinates: true,
+			knn: 3,
+			phasecutoff: 200,
+			deduplicate: false,
+			merge: true,
+			wolfram: false,
+			rulendx: false
+		};
+		Object.assign( this.opt, opt );
+		if ( this.opt.maxevents === Infinity &&
+				 this.opt.maxsteps === Infinity &&
+			   this.opt.maxtokens === Infinity ) {
+			// If no max limits set, use default limits
+			this.opt.maxsteps = 50;
+			this.opt.maxevents = (this.opt.evolution || 4) * 1000;
+			this.opt.maxtokens = 20000;
+		}
+
+		this.timerid = null; // Timer
+		this.rewritedelay = 50; // Timer delay in msec
+		this.progressfn = null; // Callback for rewrite progress
+		this.finishedfn = null; // Callback for rewrite finished
+
+		this.progress = { // Real-time statistics about rewriting process
+			progress: 0,
+			step: "0",
+			matches: "0",
+			events: "0",
+			post: ""
+		};
+		this.interrupt = false; // If true, user stopped the rewriting process
+	}
+
+	/**
+	* Rewrite.
+	* @generator
+	*/
+	*prepare_computation() {
+
+		let start = Date.now();
+		do {
+			// New step
+			this.step++;
+			this.progress.step = "" + this.step;
+			this.progress.matches = "" + 0;
+
+		    let t0 = {
+		    	id: 0,
+		    	parent: [],
+		    	child: [],
+                edge: []
+		    };
+		    let t1 = {
+		    	id: 1,
+		    	parent: [],
+		    	child: [],
+                edge: [0,1,2]
+		    };
+		    let t2 = {
+		    	id: 1,
+		    	parent: [],
+		    	child: [],
+                edge: []
+		    };
+            t0.child.push(t1);
+            t1.parent.push(t0);
+            t1.child.push(t2);
+            t2.parent.push(t1);
+            // this.multiway.EV.push(t0);
+            this.EV.push(t0);
+			yield;
+
+			start = Date.now();
+			this.progress.matches = "" + this.M.length;
+			this.progress.events = "" + this.eventcnt;
+		} while(
+			!this.interrupt &&
+			(this.step < this.opt.maxsteps) &&
+			(this.eventcnt < this.opt.maxevents)
+		);
+	}
+
+	/**
+	* Timer.
+	* @param {Object} g Generator for rewrite
+	*/
+	timer(g) {
+		if ( g.next().done ) {
+			this.interrupt = false;
+			if ( this.finishedfn ) {
+				this.finishedfn();
+			}
+		} else {
+			if ( this.progressfn ) {
+				this.progress.progress = this.progress.step / this.opt.maxsteps;
+				this.progressfn( this.progress );
+			}
+			this.timerid = setTimeout( this.timer.bind(this), this.rewritedelay, g );
+		}
+	}
+
+	/**
+	* Run abstract rewriting rules.
+	* @param {Rules} rulestr Rewriting rules as a string
+	* @param {Options} opt
+	* @param {progressfn} [progressfn=null] Progress update callback function
+	* @param {finishedfn} [finishedfn=null] Rewriting finished callback function
+	*/
+	run( rulestr, opt, progressfn = null, finishedfn = null ) {
+
+		// Set parameters
+		this.reset( opt );
+		// this.rulial.setRule( rulestr );
+		this.progressfn = progressfn;
+		this.finishedfn = finishedfn;
+
+		// Start rewriting process; timeout if either of the callback fns set
+		let g = this.prepare_computation();
+		if ( this.progressfn || this.finishedfn ) {
+			this.timerid = setTimeout( this.timer.bind(this), this.rewritedelay, g );
+		} else {
+			while ( !g.next().done );
+		}
+	}
+
+	/**
+	* Cancel rewriting process.
+	*/
+	cancel() {
+		this.progress.user = "Stopping...";
+		this.interrupt = true;
+	}
+
+	/**
+	* Report status.
+	*/
+	status() {
+		return { ..."rewriter good" };
+	}
+
+	/**
 	* Display x and/or y.
 	* @param {Object} n Node object
 	*/
@@ -106,9 +256,7 @@ class Simulator extends Rewriter {
 	* @return {string} Rewriting rule in standard string format.
 	*/
 	validateRule( rulestr ) {
-		let r = new Rulial();
-		r.setRule( rulestr );
-		return r.getRule();
+		return "validate rule okay";
 	}
 
 	/**
@@ -117,21 +265,8 @@ class Simulator extends Rewriter {
 	*/
 	setRefFrame( rf ) {
 		if ( rf.hasOwnProperty("view") ) {
-			let initlen;
-			switch( rf.view ) {
-				case "time":
-					this.observer.view = 2;
-					initlen = 1;
-					break;
-				case "phase":
-					this.observer.view = 3;
-					initlen = this.rulial.initial.length;
-					this.maxtokenid = -1;
-					break;
-				default: // space
-					this.observer.view = 1;
-					initlen = this.rulial.initial.length;
-			}
+			let initlen = 1;
+			this.observer.view = 1;
 
 			// Stop animation and set position to start
 			this.stop();
@@ -157,7 +292,7 @@ class Simulator extends Rewriter {
 				if ( this.observer.view === 1 ) update = true;
 			}
 			if ( update ) {
-				this.processSpatialEvent( this.multiway.EV[ i ] );
+				this.processSpatialEvent( this.EV[ i ] );
 			}
 			this.refresh();
 		}
@@ -221,8 +356,8 @@ class Simulator extends Rewriter {
 	* @return {boolean} True there are more events to process.
 	*/
 	tick( steps = 1, reverse = false ) {
-		while ( steps > 0 && (( reverse && this.pos > 0) || ( !reverse && this.pos < this.multiway.EV.length))) {
-			let ev = reverse ? this.multiway.EV[ this.pos - 1 ] : this.multiway.EV[ this.pos ];
+		while ( steps > 0 && (( reverse && this.pos > 0) || ( !reverse && this.pos < this.EV.length))) {
+			let ev = reverse ? this.EV[ this.pos - 1 ] : this.EV[ this.pos ];
 			let changed = false;
 			changed = this.processSpatialEvent( ev, reverse);
 			if ( changed ) {
@@ -241,7 +376,7 @@ class Simulator extends Rewriter {
 		}
 		this.refresh();
 
-		return reverse ? (this.pos > 1) : (this.pos < this.multiway.EV.length);
+		return reverse ? (this.pos > 1) : (this.pos < this.EV.length);
 	}
 
 	/**
@@ -288,7 +423,8 @@ class Simulator extends Rewriter {
 	final() {
 		this.stop();
 		this.G.force(-1,10);
-		this.tick( this.multiway.EV.length );
+		// this.tick( this.multiway.EV.length );
+		this.tick( this.EV.length );
 	}
 
 	/**
@@ -296,7 +432,7 @@ class Simulator extends Rewriter {
 	* @return {Object} Status of the Multiway3D.
 	*/
 	status() {
-		return { ...this.G.status(), events: this.pos+"/"+this.multiway.EV.length };
+		return { ...this.G.status(), events: this.pos+"/"+this.EV.length };
 	}
 
 }
