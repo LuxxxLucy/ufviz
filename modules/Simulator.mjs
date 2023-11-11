c22cb71ead34912bfd3e910163fdc966b51ad903
LU Jialin <luxxxlucy@gmail.com>
2023-11-12 01:40:28 +0800

    Coding style change, prettify

--- modules/Simulator.mjs
+++ modules/Simulator.mjs
@@ -1,7 +1,6 @@
-
 /**
-* @class UnionFind
-*/
+ * @class UnionFind
+ */
 class UnionFind {
     constructor() {
         this.parents = {};
@@ -48,7 +47,11 @@ class UnionFind {
             }
         }
 
-        return { vertices: Array.from(vertices), edges , uf: this};
+        return {
+            vertices: Array.from(vertices),
+            edges,
+            uf: this
+        };
     }
 }
 
@@ -78,113 +81,116 @@ function processUnionFindInput(input) {
 }
 
 /**
-* @class User interface for Simulator
-*
-* Note: skeleton originally adopted from Mika Suominen Tuomas Sorakivi who implemented this for Hypergraph Rewrite
-* https://github.com/met4citizen/Hypergraph)
-*/
+ * @class User interface for Simulator
+ *
+ * Note: skeleton originally adopted from Mika Suominen Tuomas Sorakivi who implemented this for Hypergraph Rewrite
+ * https://github.com/met4citizen/Hypergraph)
+ */
 class Simulator {
-	/**
-	* Creates an instance of Simulator.
-	* @param {Object} element DOM element of the canvas
-	* @param {Object} status DOM element of the status
-	* @constructor
-	*/
-	constructor(canvas,status) {
-		this.reset();
+    /**
+     * Creates an instance of Simulator.
+     * @param {Object} element DOM element of the canvas
+     * @param {Object} status DOM element of the status
+     * @constructor
+     */
+    constructor(canvas, status) {
+        this.reset();
 
         this.AnimateGraph =
-                ForceGraph3D()
-                (document.getElementById('3d-graph'))
-                .numDimensions( 2 )
-                .showNavInfo( false )
-                .enablePointerInteraction( true )
-                .dagMode('bu') // bottom-up
-                .dagLevelDistance(30)
+            ForceGraph3D()
+            (document.getElementById('3d-graph'))
+            .numDimensions(2)
+            .showNavInfo(false)
+            .enablePointerInteraction(true)
+            .dagMode('bu') // bottom-up
+            .dagLevelDistance(30)
 
-                .linkDirectionalArrowLength(10.5)
-                .linkDirectionalArrowRelPos(1)
-                .linkCurvature(0.25)
-                .linkWidth(3)
-                .linkColor("grey")
-                .linkCurvature('curvature')
-                .linkCurveRotation('rotation')
-                .linkDirectionalParticles(2)
-                .linkDirectionalParticleWidth(0.8)
-                .linkDirectionalParticleSpeed(0.006)
+            .linkDirectionalArrowLength(10.5)
+            .linkDirectionalArrowRelPos(1)
+            .linkCurvature(0.25)
+            .linkWidth(3)
+            .linkColor("grey")
+            .linkCurvature('curvature')
+            .linkCurveRotation('rotation')
+            .linkDirectionalParticles(2)
+            .linkDirectionalParticleWidth(0.8)
+            .linkDirectionalParticleSpeed(0.006)
 
-                .nodeOpacity(1)
-                .nodeThreeObject(node => {
-                    const sprite = new SpriteText(node.id);
-                    sprite.material.depthWrite = false; // make sprite background transparent
-                    sprite.color = "white";
-                    sprite.textHeight = 8;
-                    return sprite;
-                })
-                .graphData({nodes: [], links: []})
+            .nodeOpacity(1)
+            .nodeThreeObject(node => {
+                const sprite = new SpriteText(node.id);
+                sprite.material.depthWrite = false; // make sprite background transparent
+                sprite.color = "white";
+                sprite.textHeight = 8;
+                return sprite;
+            })
+            .graphData({
+                nodes: [],
+                links: []
+            })
 
-		this.dom = status; // DOM element for status
+        this.dom = status; // DOM element for status
 
         this.pos = 0; // Event log position
-		this.playpos = 0; // Play position
-		this.playing = false; // Is play on?
-		this.stopfn = null; // stop callback function
+        this.playpos = 0; // Play position
+        this.playing = false; // Is play on?
+        this.stopfn = null; // stop callback function
 
-		this.H = new Map(); // Highlights
-		this.F = null; // Fields
+        this.H = new Map(); // Highlights
+        this.F = null; // Fields
 
-		// Variables x and y
-		this.x = 0;
-		this.y = 0;
-	}
+        // Variables x and y
+        this.x = 0;
+        this.y = 0;
+    }
 
-	/**
-	* Reset instance.
-	* @param {Options} [opt=null]
-	*/
-	reset( opt = null ) {
-		opt = opt || {};
+    /**
+     * Reset instance.
+     * @param {Options} [opt=null]
+     */
+    reset(opt = null) {
+        opt = opt || {};
 
         this.EV = []; // graphs
-		this.step = 0;
-		this.eventcnt = 0;
-		this.opt = {
-			maxsteps: Infinity,
-		};
-		Object.assign( this.opt, opt );
-		if (this.opt.maxsteps === Infinity) {
-			// If no max limits set, use default limits
-			this.opt.maxsteps = 1000;
-		}
-
-		this.timerid = null; // Timer
-		this.rewritedelay = 50; // Timer delay in msec
-		this.progressfn = null; // Callback for rewrite progress
-		this.finishedfn = null; // Callback for rewrite finished
-
-		this.progress = { // Real-time statistics about rewriting process
-			progress: 0,
-			step: "0",
-			matches: "0",
-			events: "0",
-			post: ""
-		};
-		this.interrupt = false; // If true, user stopped the rewriting process
-	}
-
-	/**
-	* Rewrite.
-	* @generator
-	*/
-	*prepare_computation() {
-
-		let start = Date.now();
         this.step = 0;
-		do {
-			// New step
-			this.step++;
-			this.progress.step = "" + this.step;
-			this.progress.matches = "" + 0;
+        this.eventcnt = 0;
+        this.opt = {
+            maxsteps: Infinity,
+        };
+        Object.assign(this.opt, opt);
+        if (this.opt.maxsteps === Infinity) {
+            // If no max limits set, use default limits
+            this.opt.maxsteps = 1000;
+        }
+
+        this.timerid = null; // Timer
+        this.rewritedelay = 50; // Timer delay in msec
+        this.progressfn = null; // Callback for rewrite progress
+        this.finishedfn = null; // Callback for rewrite finished
+
+        this.progress = { // Real-time statistics about rewriting process
+            progress: 0,
+            step: "0",
+            matches: "0",
+            events: "0",
+            post: ""
+        };
+        this.interrupt = false; // If true, user stopped the rewriting process
+    }
+
+    /**
+     * Rewrite.
+     * @generator
+     */
+    * prepare_computation() {
+
+        let start = Date.now();
+        this.step = 0;
+        do {
+            // New step
+            this.step++;
+            this.progress.step = "" + this.step;
+            this.progress.matches = "" + 0;
 
             const uf = new UnionFind();
 
@@ -192,7 +198,7 @@ class Simulator {
             const pairs = processUnionFindInput(input);
 
             const numberOfStep = this.step;
-            
+
             if (Array.isArray(pairs)) {
                 // Iterate only over the first 'numberOfStep' pairs
                 for (let i = 0; i < Math.min(numberOfStep, pairs.length); i++) {
@@ -201,152 +207,154 @@ class Simulator {
             } else {
                 console.error(pairs); // Log error message if the input is invalid
             }
-    
+
             const graph = uf.getGraph();
             this.EV.push(graph);
-			yield;
+            yield;
 
-			start = Date.now();
-			this.progress.events = "" + this.eventcnt;
-		} while(
-			!this.interrupt &&
-			(this.step < this.opt.maxsteps)
-		);
-	}
+            start = Date.now();
+            this.progress.events = "" + this.eventcnt;
+        } while (
+            !this.interrupt &&
+            (this.step < this.opt.maxsteps)
+        );
+    }
 
-	/**
-	* Timer.
-	* @param {Object} g Generator for rewrite
-	*/
-	timer(g) {
-		if ( g.next().done ) {
-			this.interrupt = false;
-			if ( this.finishedfn ) {
-				this.finishedfn();
-			}
-		} else {
-			if ( this.progressfn ) {
-				this.progress.progress = this.progress.step / this.opt.maxsteps;
-				this.progressfn( this.progress );
-			}
-			this.timerid = setTimeout( this.timer.bind(this), this.rewritedelay, g );
-		}
-	}
+    /**
+     * Timer.
+     * @param {Object} g Generator for rewrite
+     */
+    timer(g) {
+        if (g.next().done) {
+            this.interrupt = false;
+            if (this.finishedfn) {
+                this.finishedfn();
+            }
+        } else {
+            if (this.progressfn) {
+                this.progress.progress = this.progress.step / this.opt.maxsteps;
+                this.progressfn(this.progress);
+            }
+            this.timerid = setTimeout(this.timer.bind(this), this.rewritedelay, g);
+        }
+    }
 
-	/**
-	* Run abstract rewriting rules.
-	* @param {Rules} rulestr Rewriting rules as a string
-	* @param {Options} opt
-	* @param {progressfn} [progressfn=null] Progress update callback function
-	* @param {finishedfn} [finishedfn=null] Rewriting finished callback function
-	*/
-	run( rulestr, opt, progressfn = null, finishedfn = null ) {
+    /**
+     * Run abstract rewriting rules.
+     * @param {Rules} rulestr Rewriting rules as a string
+     * @param {Options} opt
+     * @param {progressfn} [progressfn=null] Progress update callback function
+     * @param {finishedfn} [finishedfn=null] Rewriting finished callback function
+     */
+    run(rulestr, opt, progressfn = null, finishedfn = null) {
 
-		// Set parameters
-		this.reset( opt );
-		this.progressfn = progressfn;
-		this.finishedfn = finishedfn;
+        // Set parameters
+        this.reset(opt);
+        this.progressfn = progressfn;
+        this.finishedfn = finishedfn;
 
-		// Start rewriting process; timeout if either of the callback fns set
-		let g = this.prepare_computation();
-		if ( this.progressfn || this.finishedfn ) {
-			this.timerid = setTimeout( this.timer.bind(this), this.rewritedelay, g );
-		} else {
-			while ( !g.next().done );
-		}
-	}
+        // Start rewriting process; timeout if either of the callback fns set
+        let g = this.prepare_computation();
+        if (this.progressfn || this.finishedfn) {
+            this.timerid = setTimeout(this.timer.bind(this), this.rewritedelay, g);
+        } else {
+            while (!g.next().done);
+        }
+    }
 
-	/**
-	* Cancel rewriting process.
-	*/
-	cancel() {
-		this.progress.user = "Stopping...";
-		this.interrupt = true;
-	}
+    /**
+     * Cancel rewriting process.
+     */
+    cancel() {
+        this.progress.user = "Stopping...";
+        this.interrupt = true;
+    }
 
-	/**
-	* Report status.
-	*/
-	status() {
-		return { ..."rewriter good" };
-	}
+    /**
+     * Report status.
+     */
+    status() {
+        return {
+            ..."rewriter good"
+        };
+    }
 
-	/**
-	* Display x and/or y.
-	* @param {Object} n Node object
-	*/
-	static nodeThreeObject( n ) {
-		if ( n.id === this.x ) {
-			if ( n.id === this.y ) {
-				n.big = true;
-				return new SpriteText("x=y",26,"DarkSlateGray");
-			} else {
-				n.big = true;
-				return new SpriteText("x",26,"DarkSlateGray");
-			}
-		} else if ( n.id === this.y ) {
-			n.big = true;
-			return new SpriteText("y",26,"DarkSlateGray");
-		} else {
-			n.big = false;
-			return false;
-		}
-	}
+    /**
+     * Display x and/or y.
+     * @param {Object} n Node object
+     */
+    static nodeThreeObject(n) {
+        if (n.id === this.x) {
+            if (n.id === this.y) {
+                n.big = true;
+                return new SpriteText("x=y", 26, "DarkSlateGray");
+            } else {
+                n.big = true;
+                return new SpriteText("x", 26, "DarkSlateGray");
+            }
+        } else if (n.id === this.y) {
+            n.big = true;
+            return new SpriteText("y", 26, "DarkSlateGray");
+        } else {
+            n.big = false;
+            return false;
+        }
+    }
 
-	/**
-	* Check rewriting rule by passing it to algorithmic parser.
-	* @param {string} rulestr Rewriting rule in string format.
-	* @return {string} Rewriting rule in standard string format.
-	*/
-	validateRule( rulestr ) {
-		return "validate rule okay";
-	}
+    /**
+     * Check rewriting rule by passing it to algorithmic parser.
+     * @param {string} rulestr Rewriting rule in string format.
+     * @return {string} Rewriting rule in standard string format.
+     */
+    validateRule(rulestr) {
+        return "validate rule okay";
+    }
 
-	/**
-	* @param {RefFrame} rf
-	*/
-	setRefFrame( rf ) {
-		let initlen = 0;
+    /**
+     * @param {RefFrame} rf
+     */
+    setRefFrame(rf) {
+        let initlen = 0;
 
-		// Stop animation and set position to start
-		this.stop();
-		this.pos = 0;
-		this.playpos = 0;
+        // Stop animation and set position to start
+        this.stop();
+        this.pos = 0;
+        this.playpos = 0;
 
-		// Reset step
-		this.step=0;
+        // Reset step
+        this.step = 0;
 
-		// First additions
-		this.tick( initlen );
-	}
+        // First additions
+        this.tick(initlen);
+    }
 
-	/**
-	* Refresh UI.
-	*/
-	refresh() {
-		if ( this.dom ) {
-			let s = this.status();
-			let str = "";
-			Object.keys(s).forEach( k => {
-				str += '<span class="label up">'+k+'</span><span class="label status">'+s[k]+'</span>';
-			});
-			this.dom.innerHTML = str;
-		}
-	}
+    /**
+     * Refresh UI.
+     */
+    refresh() {
+        if (this.dom) {
+            let s = this.status();
+            let str = "";
+            Object.keys(s).forEach(k => {
+                str += '<span class="label up">' + k + '</span><span class="label status">' + s[k] + '</span>';
+            });
+            this.dom.innerHTML = str;
+        }
+    }
 
-	/**
-	* Show of hide edges in spatial graph.
-	* @param {Object} ev Event reference
-	* @param {boolean} [reverse=false] If true, reverse the process of add and remove nodes
-	* @return {boolean} True, if some was made
-	*/
-	processSpatialEvent( ev, reverse = false ) {
-        let gData =
-            {
-            nodes: ev.vertices.map(i => ({ id: i }))
-            ,
+    /**
+     * Show of hide edges in spatial graph.
+     * @param {Object} ev Event reference
+     * @param {boolean} [reverse=false] If true, reverse the process of add and remove nodes
+     * @return {boolean} True, if some was made
+     */
+    processSpatialEvent(ev, reverse = false) {
+        let gData = {
+            nodes: ev.vertices.map(i => ({
+                id: i
+            })),
             links: ev.edges.map(
-                (a,b) => ({
+                (a, b) => ({
                     source: a[0],
                     target: a[1],
                     target_node: {
@@ -358,102 +366,106 @@ class Simulator {
         };
         let change = true;
         this.AnimateGraph
-                .graphData(gData)
-        ;
-		return change;
-	}
+            .graphData(gData);
+        return change;
+    }
 
-	/**
-	* Process events.
-	* @param {number} [steps=1] Number of steps to process
-	* @param {boolean} [reverse=false] If true, reverse the process of add and remove nodes
-	* @return {boolean} True there are more events to process.
-	*/
-	tick( steps = 1, reverse = false ) {
-		while ( steps > 0 && (( reverse && this.pos > 0) || ( !reverse && this.pos < this.EV.length))) {
-			if (reverse) {
-				this.pos--;
-			} else {
-				this.pos++;
-			}
+    /**
+     * Process events.
+     * @param {number} [steps=1] Number of steps to process
+     * @param {boolean} [reverse=false] If true, reverse the process of add and remove nodes
+     * @return {boolean} True there are more events to process.
+     */
+    tick(steps = 1, reverse = false) {
+        while (steps > 0 && ((reverse && this.pos > 0) || (!reverse && this.pos < this.EV.length))) {
+            if (reverse) {
+                this.pos--;
+            } else {
+                this.pos++;
+            }
             console.log("this time pos,", this.pos, "revser", reverse);
 
-            let ev = this.pos === 0 ? 
-                 {vertices:[], edges:[], uf: []} :
-			    this.EV[ this.pos - 1 ] ;
-                 
+            let ev = this.pos === 0 ? {
+                    vertices: [],
+                    edges: [],
+                    uf: []
+                } :
+                this.EV[this.pos - 1];
 
-			let changed = false;
-			changed = this.processSpatialEvent( ev, reverse);
-			if ( changed ) {
-				steps--;
-				if ( reverse ) {
-					this.playpos = 0;
-				} else {
-					this.playpos++;
-				}
-			}
-		}
-		this.refresh();
 
-		return reverse ? (this.pos > 0) : (this.pos < this.EV.length);
-	}
+            let changed = false;
+            changed = this.processSpatialEvent(ev, reverse);
+            if (changed) {
+                steps--;
+                if (reverse) {
+                    this.playpos = 0;
+                } else {
+                    this.playpos++;
+                }
+            }
+        }
+        this.refresh();
 
-	/**
-	* Timed update process.
-	*/
-	update() {
-		const steps = Math.min( 15, Math.ceil( ( this.playpos + 1 ) / 10) );
-		if ( this.tick( steps ) ) {
-			if ( this.playing ) setTimeout( this.update.bind(this), 250 );
-		} else {
-			this.stop();
-			if ( this.stopfn ) this.stopfn();
-		}
-	}
+        return reverse ? (this.pos > 0) : (this.pos < this.EV.length);
+    }
 
-	/**
-	* Callback for animation end.
-	* @callback stopcallbackfn
-	*/
+    /**
+     * Timed update process.
+     */
+    update() {
+        const steps = Math.min(15, Math.ceil((this.playpos + 1) / 10));
+        if (this.tick(steps)) {
+            if (this.playing) setTimeout(this.update.bind(this), 250);
+        } else {
+            this.stop();
+            if (this.stopfn) this.stopfn();
+        }
+    }
 
-	/**
-	* Play animation.
-	* @param {stopcallbackfn} stopcallbackfn Animation stopped callback function
-	*/
-	play( stopcallbackfn = null ) {
-		this.stopfn = stopcallbackfn;
-		this.playpos = 0;
-		this.playing = true;
-		this.update();
-	}
+    /**
+     * Callback for animation end.
+     * @callback stopcallbackfn
+     */
 
-	/**
-	* Stop animation.
-	*/
-	stop() {
-		this.playing = false;
-	}
+    /**
+     * Play animation.
+     * @param {stopcallbackfn} stopcallbackfn Animation stopped callback function
+     */
+    play(stopcallbackfn = null) {
+        this.stopfn = stopcallbackfn;
+        this.playpos = 0;
+        this.playing = true;
+        this.update();
+    }
 
-	/**
-	* Skip to the end of the animation.
-	*/
-	final() {
-		this.stop();
-		this.tick( this.EV.length );
-	}
+    /**
+     * Stop animation.
+     */
+    stop() {
+        this.playing = false;
+    }
 
-	/**
-	* Report status.
-	* @return {Object} Status of the Multiway3D.
-	*/
-	status() {
-		return {
-            nodes: this.pos === 0 ? 0 : this.EV[ this.pos - 1 ].vertices.length,
-            edges: this.pos === 0 ? 0 : this.EV[ this.pos - 1 ].edges.length,
-            events: this.pos+"/"+this.EV.length
+    /**
+     * Skip to the end of the animation.
+     */
+    final() {
+        this.stop();
+        this.tick(this.EV.length);
+    }
+
+    /**
+     * Report status.
+     * @return {Object} Status of the Multiway3D.
+     */
+    status() {
+        return {
+            nodes: this.pos === 0 ? 0 : this.EV[this.pos - 1].vertices.length,
+            edges: this.pos === 0 ? 0 : this.EV[this.pos - 1].edges.length,
+            events: this.pos + "/" + this.EV.length
         };
-	}
+    }
 }
 
-export { Simulator };
+export {
+    Simulator
+};
